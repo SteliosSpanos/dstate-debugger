e7636d379ef330da5abf6f85260d7b11b8894440
SteliosSpanos <stylspan19@gmail.com>
2026-02-06 00:04:17 +0200

    Adds for helper function implementations

--- src/proc_utils.c
+++ src/proc_utils.c
@@ -42,3 +42,68 @@ ssize_t read_proc_file(const char *path, char *buffer, size_t bufsize)
     close(fp);
     return total;
 }
+
+ssize_t read_proc_line(const char *path, char *buffer, size_t bufsize)
+{
+
+    FILE *fp;
+
+    fp = fopen(path, "r");
+    if (!fp)
+        return -1;
+
+    if (fgets(buffer, bufsize, fp) == NULL)
+    {
+        fclose(fp);
+        return -1;
+    }
+
+    fclose(fp);
+
+    ssize_t len = strlen(buffer);
+    if (len > 0 && buffer[len - 1] == '\n')
+    {
+        buffer[len - 1] = '\0';
+        len--;
+    }
+
+    return len;
+}
+
+ssize_t read_proc_link(const char *path, char *buffer, size_t bufsize)
+{
+    ssize_t len;
+
+    len = readlink(path, buffer, bufsize - 1);
+
+    if (len < 0)
+    {
+        buffer[0] = '\0';
+        return -1;
+    }
+
+    buffer[bufsize] = '\0';
+    return len;
+}
+
+char *build_proc_path(char *buffer, size_t bufsize, pid_t pid, const char *file)
+{
+    int n;
+
+    n = snprintf(buffer, bufsize, "/proc/%d/%s", pid, file);
+
+    if (n < 0 || (size_t)n >= bufsize)
+        return NULL;
+
+    return buffer;
+}
+
+int pid_exists(pid_t pid)
+{
+    char path[32];
+    struct stat st;
+
+    snprintf(path, sizeof(path), "/proc/%d", pid);
+
+    return (stat(path, &st) == 0);
+}
