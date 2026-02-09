e2020b704477a6c8dedab352af4a1abadef4c0ac
SteliosSpanos <stylspan19@gmail.com>
2026-02-09 23:03:24 +0200

    Deletes unecessary

--- include/proc_utils.h
+++ include/proc_utils.h
@@ -3,7 +3,6 @@
 
 #include <sys/types.h>
 #include <stddef.h>
-#include <stdint.h>
 
 ssize_t read_proc_file(const char *path, char *buffer, size_t bufsize);
 
@@ -12,19 +11,5 @@ ssize_t read_proc_line(const char *path, char *buffer, size_t bufsize);
 ssize_t read_proc_link(const char *path, char *buffer, size_t bufsize);
 
 char *build_proc_path(char *buffer, size_t bufsize, pid_t pid, const char *file);
-
-int pid_exists(pid_t pid);
-
-int parse_status_line(const char *line, char *key, size_t keylen, char *value, size_t vallen);
-
-double ticks_to_seconds(unsigned long ticks);
-
-void format_size(unsigned long bytes, char *buffer, size_t bufsize);
-
-uint64_t get_timestamp();
-
-int is_root();
-
-int has_ptrace_capability();
 
 #endif
